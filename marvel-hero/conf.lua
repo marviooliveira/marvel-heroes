1142e30a659e32075eb5a53fe95372aa8a41bda5
marvel <40925616+marviooliveira@users.noreply.github.com>
2018-07-08 06:13:42 -0300

    ultima atualizacao do dia
    so ajeitei  o skip

---
+++ marvel-hero/conf.lua
@@ -0,0 +1,13 @@
+function love.conf(t)
+	t.window.width  = 1000
+	t.window.height = 600
+	t.window.title  = "MARVEL HEROES" 
+	t.window.resizable = false
+	t.version = "0.10.2"
+	t.console = false
+	t.accelerometerjoystick = true
+	t.externalstorage = false
+	t.window.icon = "img/icone.png"
+	--fullscreentype="desktop"
+
+end
